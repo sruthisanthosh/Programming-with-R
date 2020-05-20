75e6913a2f4344e774716af57a48bf0b6157964e
Sruthi Santhosh <35186002+sruthisanthosh@users.noreply.github.com>
2020-05-20 16:22:31 +0530

    Add files via upload

---
+++ loop.R
@@ -0,0 +1,16 @@
+#while loop
+
+while(TRUE){
+  print("Hello!")
+}
+
+counter<- 1
+while(counter<12){
+  print(counter)
+  counter<-counter+1
+}
+
+
+for(i in 1:5){
+  print("Hello R")
+}
---
+++ usingvariables.R
@@ -0,0 +1,19 @@
+A <- 10
+B <- 5
+C <- A+B
+D<-A-B
+C
+
+#variable1
+var1<-2.5
+var2<-4
+
+result<-var1/var2
+result
+
+answer<-sqrt(var2)
+answer<-sqrt(var1)
+
+greeting<-"Hello"
+name<-"Bob"
+message<-paste(greeting,name)
