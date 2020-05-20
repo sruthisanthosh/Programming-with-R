cd9d8e6a6a98f5bcb3d87c88085dc7df34fdeaa8
Sruthi Santhosh <35186002+sruthisanthosh@users.noreply.github.com>
2020-05-20 16:26:12 +0530

    Update loop.R

--- loop.R
+++ loop.R
@@ -4,13 +4,13 @@ while(TRUE){
   print("Hello!")
 }
 
-counter<- 1
+counter<- 1             #using while as a counter
 while(counter<12){
   print(counter)
   counter<-counter+1
 }
 
-
+#for loop
 for(i in 1:5){
   print("Hello R")
 }
