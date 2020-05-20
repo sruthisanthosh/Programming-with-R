7f69f8a4c154319b3e43eb357282b09094e51ad0
Sruthi Santhosh <35186002+sruthisanthosh@users.noreply.github.com>
2020-05-20 16:25:27 +0530

    Update usingvariables.R

--- usingvariables.R
+++ usingvariables.R
@@ -2,13 +2,12 @@ A <- 10
 B <- 5
 C <- A+B
 D<-A-B
-C
 
 #variable1
 var1<-2.5
 var2<-4
 
-result<-var1/var2
+result <- var1/var2
 result
 
 answer<-sqrt(var2)
@@ -16,4 +15,4 @@ answer<-sqrt(var1)
 
 greeting<-"Hello"
 name<-"Bob"
-message<-paste(greeting,name)
+message<-paste(greeting,name)    #concatenates
