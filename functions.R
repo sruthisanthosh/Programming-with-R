626d94ac96564e1ce69c28f1b5d3f30292a901d9
Sruthi Santhosh <35186002+sruthisanthosh@users.noreply.github.com>
2020-05-20 23:26:18 +0530

    Add files via upload

---
+++ functions.R
@@ -0,0 +1,23 @@
+ #functions in R
+
+rnorm(n=5, sd=8)
+
+x<- c("a","b","c")
+c()
+A <- seq(from=10, to=20, length.out=100)
+seq(from=10, to=20, along.with=x)
+
+rep(x, each=3)   # a a a b b b c c c  (all charaters)
+rep(x,times=3)   # a b c a b c a b c 
+
+print()
+
+is.numeric()
+is.integer()
+is.double()
+is.character() 
+
+typeof()
+
+B <- sqrt(A)   #to find sqrt of a vector
+paste()
